ea5c07bb459ad3517ab7ff922144a44f83f371e7
matheus <tecnologia.matheus@gmail.com>
2022-02-15 12:56:49 -0300

    feat(my_buttons): button text color can be customized

--- packages/facily_buttons/lib/src/flat_button.dart
+++ packages/facily_buttons/lib/src/flat_button.dart
@@ -3,18 +3,25 @@ import 'package:flutter/material.dart';
 class FacilyFlatButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String text;
+  final Color textColor;
 
-  const FacilyFlatButton({
-    Key? key,
-    required this.onPressed,
-    required this.text,
-  }) : super(key: key);
+  const FacilyFlatButton(
+      {Key? key,
+      required this.onPressed,
+      required this.text,
+      this.textColor = Colors.blue})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: onPressed,
-      child: Text(text),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: textColor,
+        ),
+      ),
     );
   }
 }
